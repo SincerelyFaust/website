6badbe4deef94657b5664292eed77bb0ebcb3ffa
SincerelyFaust <SincerelyFaust@users.noreply.github.com>
2022-02-26 16:28:44 +0000

    deployed via Gatsby Publish Action 🎩 for 0961b1bc7c1712ed5113e2b0844768cf432d1148

---
+++ component---src-pages-404-jsx-4dac9a9f41a258176553.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[4],{pssB:function(e,t,n){"use strict";n.r(t);var a=n("q1tI"),o=n.n(a),u=n("Wbzz"),l=n("6uTu"),s=n("ivnd"),r="text-lg font-bold",c="underline";t.default=function(){return o.a.createElement(l.a,null,o.a.createElement(s.a,{title:"Not found"}),o.a.createElement("h1",{className:r},"404: Not Found"),o.a.createElement("p",null,"You just hit a route that doesn't exist."," ",o.a.createElement(u.Link,{className:c,to:"/"},"Return to safety"),"."))}}}]);
+//# sourceMappingURL=component---src-pages-404-jsx-4dac9a9f41a258176553.js.map
